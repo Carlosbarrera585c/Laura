cc13d2063d7c0d1ecd586a87666636313c345312
rwilcox0103 <rwilcox0103@users.noreply.github.com>
2016-03-24 10:48:41 -0500

    pagina de las ubicaciones

---
+++ ubicaciont.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Nueva Vida el Buey</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="js/jquery.jcarousel.js" type="text/javascript"></script>
+<script src="js/jquery-func.js" type="text/javascript"></script>
+<!--[if IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+</head>
+<body>
+<!-- START PAGE SOURCE -->
+<div id="header-holder">
+  <div class="shell">
+    <div id="header">
+      <div id="logo">
+        <h1><a href="#"></a></h1>
+        <p></p>
+      </div>
+      <div id="navigation">
+        <ul>
+          <li class="first active"><a href="#"><span>Inicio</span></a><span class="arrow">&nbsp;</span></li>
+          <li><a href="#"><span>Quien Somos</span></a><span class="arrow">&nbsp;</span></li>
+          <li><a href="#"><span>Recetas</span></a><span class="arrow">&nbsp;</span></li>
+          <li><a href="#"><span>Auriculo</span></a><span class="arrow">&nbsp;</span></li>
+          <li><a href="#"><span>Testimonios</span></a><span class="arrow">&nbsp;</span></li>
+          <li><a href="#"><span>Ubicacion</span></a><span class="arrow">&nbsp;</span></li>
+          <li class="last"><a href="#"><span>Contact</span></a><span class="arrow">&nbsp;</span></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="main-holder">
+  <div class="shell">
+    <div id="slider-holder">
+      <div id="slider-nav">
+        <ul>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+        </ul>
+      </div>
+      <div class="slider">
+        <ul>
+          <li>
+            <div class="image"> <img src="css/images/slider-img1.jpg" alt="" /> </div>
+            <div class="slider-cnt">
+              <h2>La Salud Atraves se la oreja</h2>
+            </div>
+          </li>
+          <li>
+            <div class="image"> <img src="css/images/slider-img2.jpg" alt="" /> </div>
+            <div class="slider-cnt">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            </div>
+          </li>
+          <li>
+            <div class="image"> <img src="css/images/slider-img3.jpg" alt="" /> </div>
+            <div class="slider-cnt">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<table id="location-map" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top" width="70%">
+<section class="responsive-iframe"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJhft2KxCE3YgRnbxW3xQbj_M&key=AIzaSyBAk7tEwKTo2CKmHdOC_2ylGkFbHcKVduI" allowfullscreen></iframe>
+<td align="left" valign="top" width="30%">
+<h3>Kissimmee</h3>
+<p>316 N John  Young Pkwy Suite #6<br />
+Kissimmee, FL <br />
+<strong>(407) 435-0022</strong></p>
+&nbsp;&nbsp;&nbsp;
+<h3>Horarios</h3>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top">Jue</span></td>
+<td align="left" valign="top">10am  a 1pm</span></td>
+<td align="left" valign="top"> 3pm  a 7pm</span></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+&nbsp;&nbsp;&nbsp;
+<table id="location-map" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top" width="70%">
+<section class="responsive-iframe"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ4zJMlRYo2YgRrUrdgq5ez4Y&key=AIzaSyBAk7tEwKTo2CKmHdOC_2ylGkFbHcKVduI" allowfullscreen></iframe>
+</td>
+<td align="left" valign="top" width="30%">
+<h3>West Palm Beach</h3>
+<p>1217 S Military Trail Suite D<br />
+West Palm Beach, FL 33415<br />
+<strong>(407) 435-0022</strong></p>
+<h3>Horarios</h3>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top"><span style="color: #000000;">Lun</span></td>
+<td align="left" valign="top">10 am a 1pm</td>
+<td align="left" valign="top">3pm a 7 pm</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<table id="location-map" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top" width="70%">
+<section class="responsive-iframe"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJmVt6KCp_54gR4jBUh80fGYE&key=AIzaSyBAk7tEwKTo2CKmHdOC_2ylGkFbHcKVduI" allowfullscreen></iframe>
+<small></small></section>
+</td>
+<td align="left" valign="top" width="30%">
+<h3>Orlando</h3>
+<p>6700 conroy rd # 130<br />
+Orlando fl 32835<br />
+<strong>(407) 435-0022</strong></p>
+<h3>Horarios</h3>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top"><span style="color: #000000;">Sab</span></td>
+<td align="left" valign="top">10 am a 2 pm</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<table id="location-map" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top" width="70%">
+<section class="responsive-iframe"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJiW33OQR054gRZxmPEQAxNrc&key=AIzaSyBAk7tEwKTo2CKmHdOC_2ylGkFbHcKVduI" allowfullscreen></iframe>
+</td>
+<td align="left" valign="top" width="30%">
+<h3>longwood</h3>
+<p>1700 kenlyn dr<br />
+longwood, Fl 32779<br />
+<strong>(407) 435-0022</strong></p>
+<h3>Horarios</h3>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td align="left" valign="top"><span style="color: #000000;">Miercole</span></td>
+<td align="left" valign="top">10am  a 1 pm</td>
+<td align="left" valign="top">3 pm a 7 pm</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+	<div id="footer-holder">
+  <div class="shell">
+    <div class="footer">
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+<!-- END PAGE SOURCE -->
+<div align=center>Copyright © 2015 <a href="Vida Nueva el Buey"> Vida Nueva el Buey</a> - Derecho Reservado</div></body>
+</html>	
+		
+		
+</html>
